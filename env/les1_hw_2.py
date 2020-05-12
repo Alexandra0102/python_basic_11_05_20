5916ee7df8fb15c2b55f44ea3300673e7627b09c
[Alexandra0102] <[suntsoy@yandex.ru]>
2020-05-12 13:56:21 +0300

    les1_hw

--- env/les1_hw_2.py
+++ env/les1_hw_2.py
@@ -11,9 +11,18 @@ while True:
     else:
         print('Неверный ввод, повторите, пожалуйста')
 
-timeH = sec // 3600
+timeH = sec // 3600 % 24
 timeM = sec // 60 % 60
 timeS = sec % 60
-time = f'Московское время: {timeH} : {timeM} : {timeS}'
+if timeS < 10:
+    time1 = f'Московское время: 0{timeH}'
+else: time1 = f'Московское время: {timeH}'
+if timeM < 10:
+    time2 = f'0{timeM}'
+else: time2 = f'{timeM}'
+if timeS < 10:
+    time3 = f'0{timeS}'
+else: time3 = f'{timeS}'
+time = time1+ ':' + time2 + ':' + time3
 print(time)
 
