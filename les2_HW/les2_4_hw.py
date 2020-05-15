db977cd0479df808be67bf0faff80790e4be82ae
[Alexandra0102] <[suntsoy@yandex.ru]>
2020-05-15 14:32:58 +0300

    les2_1_hw.py

--- les2_HW/les2_4_hw.py
+++ les2_HW/les2_4_hw.py
@@ -1,3 +1,21 @@
 '''4. Пользователь вводит строку из нескольких слов, разделённых пробелами.
 Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
 Если в слово длинное, выводить только первые 10 букв в слове.'''
+
+user_Input = input('Введите строку из нескольких слов, разделенных пробелами \n')
+long = len(user_Input)
+spaces = [0]
+
+for i in range(long):
+    if user_Input[i] == ' ':
+        spaces.append(i)
+print(spaces)
+
+for space in spaces:
+    print(space)
+
+    a = space
+    print(user_Input[a:space])
+
+
+
