500d525947331fb5df972a0896071365b11be620
[Alexandra0102] <[suntsoy@yandex.ru]>
2020-05-15 14:39:15 +0300

    les2_1_hw.py

--- les2_HW/les2_3_hw.py
+++ les2_HW/les2_3_hw.py
@@ -8,12 +8,12 @@ months = {
     3: 'Spring',
     4: 'Spring',
     5: 'Spring',
-6: 'Summer',
-7: 'Summer',
-8: 'Summer',
-9: 'Fall',
-10: 'Fall',
-11: 'Fall',
+    6: 'Summer',
+    7: 'Summer',
+    8: 'Summer',
+    9: 'Fall',
+    10: 'Fall',
+    11: 'Fall',
     12: 'Winter'}
 
 while True:
