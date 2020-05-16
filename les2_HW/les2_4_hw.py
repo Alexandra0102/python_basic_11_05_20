7036ebb67e5f10dc0859fa497c071145664c9b1d
[Alexandra0102] <[suntsoy@yandex.ru]>
2020-05-16 13:31:12 +0300

    les2_1_hw.py

--- les2_HW/les2_4_hw.py
+++ les2_HW/les2_4_hw.py
@@ -2,20 +2,28 @@
 Вывести каждое слово с новой строки. Строки необходимо пронумеровать.
 Если в слово длинное, выводить только первые 10 букв в слове.'''
 
+
+
 user_Input = input('Введите строку из нескольких слов, разделенных пробелами \n')
 long = len(user_Input)
 spaces = [0]
-
 for i in range(long):
     if user_Input[i] == ' ':
         spaces.append(i)
-print(spaces)
 
+a = 0
+j = 0
 for space in spaces:
-    print(space)
-
+    if j != 0:
+        if len(user_Input[a:space]) > 10:
+            print(f'слово номер {j}: ' + user_Input[a:a+10])
+        else:
+            print(f'слово номер {j}: ' + user_Input[a:space])
     a = space
-    print(user_Input[a:space])
-
+    j += 1
+if len(user_Input[a:len(user_Input)]) > 10:
+    print(f'слово номер {j}: ' + user_Input[a:a + 10])
+else:
+    print(f'слово номер {j}: ' + user_Input[a:len(user_Input)])
 
 
