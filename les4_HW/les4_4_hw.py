bf7f415dd4ce8a47c9f4c3c8ee702dda1ea56637
[Alexandra0102] <[suntsoy@yandex.ru]>
2020-05-25 13:16:01 +0300

    les4_1_hw.py

--- les4_HW/les4_4_hw.py
+++ les4_HW/les4_4_hw.py
@@ -11,12 +11,13 @@ from random import randint
 
 my_list = [randint(20, 50) for i in range(14)]
 print(my_list)
-
+my_list2 = []
 for el in range(len(my_list) - 1):
     for i in range(el + 1, len(my_list)):
-        if my_list[el] == my_list[i]:
-            print(my_list[i])
-new_list = []
-print(new_list.append(my_list[el]))
+        if my_list[el] == my_list[i]: my_list2.append(my_list[i])
+
+for el in my_list:
+    if el in my_list2: my_list.remove(el)
+print(my_list)
 
 
